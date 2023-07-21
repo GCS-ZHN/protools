6b3719c7d591e65dffda8eb80192a4656de95c5c
GCS-ZHN <zhang.h.n@foxmail.com>
2023-07-21 10:32:58 +0800

    chore: use fullname mode

--- protools/df2fasta.py
+++ protools/df2fasta.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import pandas as pd
 import os
 
@@ -6,7 +5,13 @@ from fasta import FASTA
 from Bio.Seq import Seq 
 from Bio.SeqRecord import SeqRecord
 
-def df2fasta(df, fasta_file, id_col: str, seq_cols: list, mode: str = 's', sep: str = ''):
+
+def df2fasta(df:pd.DataFrame,
+             fasta_file:str, 
+             id_col: str, 
+             seq_cols: list, 
+             mode: str = 'seperate', 
+             sep: str = ''):
     """
     Convert a dataframe to a fasta file.
 
@@ -21,22 +26,24 @@ def df2fasta(df, fasta_file, id_col: str, seq_cols: list, mode: str = 's', sep: 
     seq_cols : list
         The column names of the sequences.
     mode : str, optional
-        The mode of the conversion. 's' for single sequence,
-        'j' for joint sequence. The default is 's'.
+        The mode of the conversion. 'seperate' 
+        for single sequence per entry,
+        'joint' for joint sequence per entry.
+        The default is 'seperate'.
     sep : str, optional
         The separator of the joint sequence. The default is ''.
-        Only works when mode is 'j'.
+        Only works when mode is 'joint'.
     """
     fasta_file = os.path.abspath(fasta_file)
     with FASTA(fasta_file) as f:
         for _, row in df.iterrows():
-            if mode == 's':
+            if mode == 'seperate':
                 for seq_col in seq_cols:
                     seq_id = f"{row[id_col]}_{seq_col}"
                     seq = Seq(row[seq_col])
                     seq_record = SeqRecord(seq, id=seq_id, description='')
                     f.add_seq(seq_record)
-            elif mode == 'j':
+            elif mode == 'joint':
                 seq = Seq(sep.join([row[seq_col] for seq_col in seq_cols]))
                 seq_id = row[id_col]
                 seq_record = SeqRecord(seq, id=seq_id, description='')
@@ -46,12 +53,34 @@ def df2fasta(df, fasta_file, id_col: str, seq_cols: list, mode: str = 's', sep: 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('-i', '--input', required=True, help='input csv file')
-    parser.add_argument('-o', '--output', required=True, help='output fasta file')
-    parser.add_argument('-c', '--id_col', required=True,  help='column name of the id')
-    parser.add_argument('-s', '--seq_cols', required=True, nargs='+', help='column names of the sequences')
-    parser.add_argument('-m', '--mode', default='s', help='mode of the conversion')
-    parser.add_argument('--sep', default='', help='separator of the joint sequence')
+    parser.add_argument(
+        '-i', '--input',
+        required=True,
+        help='input csv file')
+    parser.add_argument(
+        '-o', '--output',
+        required=True,
+        help='output fasta file')
+    parser.add_argument(
+        '-c', '--id_col',
+        required=True,
+        help='column name of the id')
+    parser.add_argument(
+        '-s', '--seq_cols',
+        required=True,
+        nargs='+',
+        help='column names of the sequences')
+    parser.add_argument(
+        '--mode',
+        default='seperate',
+        choices=['seperate', 'joint'],
+        help='mode of the conversion, \
+        s means single sequence per entry, \
+        j means joint sequence per entry')
+    parser.add_argument(
+        '--sep',
+        default='',
+        help='separator of the joint sequence')
     args = parser.parse_args()
 
     df = pd.read_csv(args.input)
