e14434829aa168c9637c1312319bfc1010ca9626
GCS-ZHN <zhang.h.n@foxmail.com>
2023-11-10 10:50:05 +0800

    chore: recognize isoform

--- protools/database/uniprot.py
+++ protools/database/uniprot.py
@@ -45,7 +45,9 @@ def extract_features(record: SwissProt.Record):
 @catch_error(loger, err_types=(Exception,))
 @max_retry(max=5)
 def fetch_uniprot_features(accession_id: str, output_path: FilePath, skip_exist: bool = False):
-    if not accession_id.isalnum():
+    main_id = accession_id.split('-')[0]
+    isoform = accession_id.split('-')[1] if len(accession_id.split('-')) > 1 else ''
+    if not main_id.isalnum():
         raise ValueError(f'Invalid accession id: "{accession_id}"')
     output_path = ensure_path(output_path)
     output_path.mkdir(exist_ok=True, parents=True)
