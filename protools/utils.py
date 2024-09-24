14c1efb77f273185191620350a68d59517af10ab
Zhang.H.N <zhang.h.n@foxmail.com>
2024-09-24 14:15:41 +0800

    feat: add `Intevals` utils for Interval patterns operation

--- protools/utils.py
+++ protools/utils.py
@@ -5,9 +5,10 @@ import subprocess
 import asyncio
 import functools
 import logging
+import warnings
 
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List, Callable
 from io import IOBase
 from .typedef import FilePathType, FilePathOrIOType
 from collections import namedtuple
@@ -372,3 +373,253 @@ def local_cwd(path: FilePathType):
     yield path
     if cwd != path:
         os.chdir(cwd)
+
+
+class Intervals(object):
+    """
+    A class to represent intervals, such as "1-10, 20-30, 40-50".
+    It can be used to represent the slices of a sequence. Interval
+    will be merged if they are overlapped.
+
+
+    Parameters
+    ----------
+    intervals_pattern : str
+        The pattern of the intervals, such as "1-10, 20-30, 40-50".
+        Negative values are not supported. If start is emtpy, it
+        will be regarded as first position. If stop is empty, it
+        will be regarded as infinite value. Each interval is separated
+        by comma.
+    zero_based : bool, optional
+        Whether the interval is zero-based. Default is False.
+        First position is zero if set it to True, otherwise,
+        first position will be one.
+    end_inclusive : bool, optional
+        Whether the end of the interval is inclusive. Default is True.
+        If it is True, the end of the interval will be included in the
+        interval, otherwise, it will be excluded.
+    interval_hook : Callable[[str], str], optional
+        A hook to process each interval pattern. It will be called
+        before parsing each interval pattern. It is useful when the
+        interval pattern is not standard. For example, the interval
+        pattern is "1-10(strong), 20-30(weakly), 40-50" The hook can be
+        used to remove the content in the bracket.
+
+    Examples
+    ----------
+    >>> Intervals('1-10, 20-30, 40-50')
+    '1-10,20-30,40-50' (zero_based=False, end_inclusive=True, dynamic=False)
+    >>> Intervals.from_slices([slice(5, None), slice(25, 35)])
+    '6-' (zero_based=False, end_inclusive=True, dynamic=True)
+    """
+
+    def __init__(self,
+                 intervals_pattern: str,
+                 zero_based: bool = False,
+                 end_inclusive: bool = True,
+                 interval_hook: Optional[Callable[[str], str]] = None):
+        self.dynamic = False
+        self.zero_based = zero_based
+        self.end_inclusive = end_inclusive
+        self._interval_slices = self._parse_intervals(
+            intervals_pattern, interval_hook)
+
+    @classmethod
+    def from_slices(cls,
+                    slices: List[slice],
+                    zero_based: bool = False,
+                    end_inclusive: bool = True):
+        """
+        Create Intervals from slice list.
+        """
+        interval = cls('', 
+                       zero_based=zero_based,
+                       end_inclusive=end_inclusive)
+        interval._interval_slices = cls._merge_intervals(slices)
+        interval.dynamic = any([i.stop is None for i in interval._interval_slices])
+        return interval
+
+    @staticmethod
+    def _merge_intervals(interval_slices: List[slice]) -> List[slice]:
+        interval_slices.sort(key=lambda x: x.start)
+        merged_interval_slices = []
+        for interval in interval_slices:
+            if not merged_interval_slices:
+                merged_interval_slices.append(interval)
+            else:
+                last_interval = merged_interval_slices[-1]
+                if  last_interval.stop is None or interval.start <= last_interval.stop:
+                    if last_interval.stop is None or interval.stop is None:
+                        stop = None
+                    else:
+                        stop = max(last_interval.stop, interval.stop)
+                    merged_interval_slices[-1] = slice(last_interval.start, stop)
+                else:
+                    merged_interval_slices.append(interval)
+        return merged_interval_slices
+
+    def _convert_interval(self, start: int, stop: int) -> tuple:
+        if not self.zero_based:
+            start = 0 if start is None else start - 1
+            if stop is not None:
+                stop -= 1
+        if self.end_inclusive:
+            if stop is not None:
+                stop += 1
+        assert start >= 0, "Invalid interval: negative value is not supported"
+        if stop is not None:
+            assert stop > start, "Invalid interval: stop should not less than start"
+        return start, stop
+
+    def _parse_intervals(self, 
+                       intervals_patttern: str,
+                       interval_hook: Optional[Callable[[str], str]] = None) -> List[slice]:
+        interval_slices = []
+        for i in intervals_patttern.split(','):
+            if interval_hook:
+                i = interval_hook(i)
+            i = i.strip()
+            if not i:
+                continue
+            items = i.split('-')
+            items = [int(x) if x else None for x in items]
+            if len(items) == 1:
+                start, stop = items[0], items[0]
+            elif len(items) == 2:
+                start, stop = items
+            else:
+                raise ValueError(f"Invalid intervals: {intervals_patttern}")
+            start, stop = self._convert_interval(start, stop)
+            if stop is None:
+                self.dynamic = True
+            interval_slices.append(slice(start, stop))
+        return self._merge_intervals(interval_slices)
+
+    def intersect(self, intervals: 'Intervals') -> 'Intervals':
+        """
+        Calculate the intersection of two intervals.
+        """
+        new_interval_slices = []
+        dynamic = False
+        for i1 in self._interval_slices:
+            for i2 in intervals._interval_slices:
+                start = max(i1.start, i2.start)
+                if i1.stop is None:
+                    stop = i2.stop
+                elif i2.stop is None:
+                    stop = i1.stop
+                else:
+                    stop = min(i1.stop, i2.stop)
+                if stop is None or start < stop:
+                    new_interval_slices.append(slice(start, stop))
+                if stop is None:
+                    dynamic = True
+        res = self.__class__(
+            '',
+            zero_based=self.zero_based,
+            end_inclusive=self.end_inclusive)
+        res._interval_slices = new_interval_slices
+        res.dynamic = dynamic
+        return res
+
+    def reverse(self, low: int = None, high: int = None) -> 'Intervals':
+        """
+        Reverse the intervals.
+
+        Parameters
+        ----------
+        low : int, optional
+            The lower bound of the interval. should not less than
+            first position. If it is None, it will be regarded as
+            the first position (0 or 1 depends on `zero_based`).
+        high : int, optional
+            The upper bound of the interval.
+
+        """
+        cls = self.__class__
+        new_interval = cls('', 
+            zero_based=self.zero_based,
+            end_inclusive=self.end_inclusive)
+        intervals = []
+        start_pos = 0
+        for i in self._interval_slices:
+            if start_pos is None:
+                break
+            if i.start > start_pos:
+                intervals.append(slice(start_pos, i.start))
+            start_pos = i.stop
+
+        if start_pos is not None:
+            intervals.append(slice(start_pos, None))
+
+        new_interval._interval_slices = intervals
+        low, high = self._convert_interval(low, high)
+        bound_interval = cls.from_slices(
+            [slice(low, high)],
+            zero_based=self.zero_based,
+            end_inclusive=self.end_inclusive)
+        new_interval = new_interval.intersect(bound_interval)
+        return new_interval
+
+    def __str__(self):
+        patterns = []
+        for interval in self._interval_slices:
+            start, stop = interval.start, interval.stop
+            if not self.zero_based:
+                start += 1
+                if stop is not None:
+                    stop += 1
+            if self.end_inclusive and stop is not None:
+                stop -= 1
+            start = str(start)
+            stop = '' if stop is None else str(stop)
+            if  start == stop:
+                patterns.append(str(start))
+            else:
+                patterns.append(f"{start}-{stop}")
+        return ','.join(patterns)
+
+    def __repr__(self):
+        return f"'{self}' (zero_based={self.zero_based}, end_inclusive={self.end_inclusive}, dynamic={self.dynamic})"
+
+    def __len__(self):
+        if self.dynamic:
+            raise ValueError("Dynamic interval does not have length")
+        return sum([i.stop - i.start for i in self._interval_slices])
+
+    def __eq__(self, other: 'Intervals') -> bool:
+        return self._interval_slices == other._interval_slices
+
+    def __add__(self, shift: int) -> 'Intervals':
+        assert isinstance(shift, int), "Shift must be an integer"
+        new_intervals = self.__class__('',
+                                zero_based=self.zero_based,
+                                end_inclusive=self.end_inclusive)
+        new_interval_slices = []
+        dynamic = False
+        for i in self._interval_slices:
+            new_start = i.start + shift
+            if new_start < 0:
+                new_start = 0
+                warnings.warn(
+                    f"Shift {shift} is too large for interval {self}, start is set to 0")
+            if i.stop is None:
+                new_stop = None
+                dynamic = True
+            else:
+                new_stop = i.stop + shift
+                if new_stop < 0:
+                    new_stop = 0
+                    warnings.warn(
+                        f"Shift {shift} is too large for interval {self}, stop is set to 0")
+            if new_stop is None or new_start < new_stop:
+                new_interval_slices.append(slice(new_start, new_stop))
+        new_intervals._interval_slices = new_interval_slices
+        new_intervals.dynamic = dynamic
+        return new_intervals
+
+    def __sub__(self, shift: int) -> 'Intervals':
+        return self.__add__(-shift)
+    
+    def __iter__(self):
+        return iter(self._interval_slices)
