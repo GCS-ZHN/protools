4c417ca49441558a5011fe9007e5fb60666d5718
Zhang.H.N <zhang.h.n@foxmail.com>
2024-12-16 15:05:22 +0800

    feat: add `anno_tcr_cdr` based on anarci

--- protools/seqanno.py
+++ protools/seqanno.py
@@ -4,7 +4,9 @@ from .utils import require_package
 from .seqio import read_fasta, df2fasta
 from pathlib import Path
 try:
+    require_anarci = require_package('anarci', 'conda install -c bioconda anarci')
     require_abnumber = require_package("abnumber",  "conda install -c bioconda abnumber")
+    from anarci import run_anarci
     from abnumber import Chain, ChainParseError
 except ImportError:
     pass
@@ -14,7 +16,8 @@ _HC_ANNOTATOR = SingleChainAnnotator(chains=['H'], scheme='imgt')
 _LC_ANNOTATOR = SingleChainAnnotator(chains=['K', 'L'], scheme='imgt')
 
 
-IMGT_BORDERS = [27,  39,  56,   66,  105,  118, 129]
+IMGT_BORDERS = [27, 39, 56, 66, 105, 118, 129]
+REGION_NAMES = ['FR1', 'CDR1', 'FR2', 'CDR2', 'FR3', 'CDR3', 'FR4']
 
 
 @require_abnumber
@@ -81,7 +84,7 @@ def anno_cdr(seq: str, chain: str) -> dict:
 
     numbering, percent_identity = numbering_seq(seq, chain)
     if len(numbering) != len(seq):
-        raise ValueError(f"Numbering length {len(numbering)} != sequence length {len(seq)}, {err_message}")
+        raise ValueError(f"Numbering length {len(numbering)} != sequence length {len(seq)}")
     res = [''] * len(IMGT_BORDERS)
     real_borders = [[-1, 0] for _ in range(len(IMGT_BORDERS))]
     region_idx = 0
@@ -115,6 +118,57 @@ def anno_cdr(seq: str, chain: str) -> dict:
         f'{chain}_percent_identity': percent_identity
     }
     return res
+
+
+@require_anarci
+def anno_tcr_cdr(seq: str) -> dict:
+    """
+    Annoate TCR CDR info.
+    """
+    _, numbered, alignment_details, _ = run_anarci([('A', seq)], scheme='imgt')
+    assert len(numbered) == len(alignment_details) == 1
+    assert numbered[0] is not None, 'No domain found!'
+    assert len(numbered[0]) == len(alignment_details[0]) == 1, 'Multiple domain found!'
+    numbered_seq, fv_start, fv_end = numbered[0][0]
+    alignment_details = alignment_details[0][0]
+    chain_type = alignment_details['chain_type']
+    if chain_type not in ['B', 'A', 'D', 'G']:
+        raise RuntimeError(f'{seq} is not a valid TCR chain')
+    
+    numbered_seq = filter(lambda x: x[1] != '-', numbered_seq)
+    real_borders = [None for _ in range(len(IMGT_BORDERS))]
+    region_idx = -1
+    for seq_idx, ((aligned_idx, inscode), aa) in enumerate(numbered_seq, fv_start):
+        assert seq[seq_idx] == aa, 'AA not matched!'
+        if region_idx == -1:
+            # find first region
+            tmp_idx = 0
+            while aligned_idx >= IMGT_BORDERS[tmp_idx]:
+                tmp_idx += 1
+            region_idx = tmp_idx
+        elif aligned_idx >= IMGT_BORDERS[region_idx]:
+            region_idx += 1
+
+        if real_borders[region_idx] is None:
+            real_borders[region_idx] = [seq_idx, seq_idx + 1]
+        else:
+            real_borders[region_idx][1] = seq_idx + 1
+    
+    assert seq_idx == fv_end
+    result = {
+        'chain_type': chain_type,
+        'raw_seq': seq,
+        'fv_seq': seq[fv_start: fv_end + 1]
+        }
+    for region_idx, border in enumerate(real_borders):
+        name = REGION_NAMES[region_idx]
+        if border is None:
+            result[name] = None
+        else:
+            border = slice(*border)
+            result[name] = seq[border]
+        result[name + '_slice'] = border
+    return result
 
 
 if __name__ == "__main__":
