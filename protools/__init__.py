497007480e3374c3cd4739979d4c1cb39ec1aa94
Zhang.H.N <zhang.h.n@foxmail.com>
2025-11-12 17:11:17 +0800

    chore: add `__version__` code

--- protools/__init__.py
+++ protools/__init__.py
@@ -1,3 +1,13 @@
 """
 Protein tool collections
 """
+
+from protools.seqio import Fasta
+from protools.utils import Intervals
+
+try:
+    from ._version import get_versions # type: ignore
+    __version__ = get_versions()['version']
+    del get_versions
+except ImportError:
+    pass
