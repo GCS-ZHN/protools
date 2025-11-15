5e791152bd251b9aaf8d3f899a5614fc6c4de932
Zhang.H.N <zhang.h.n@foxmail.com>
2025-11-15 16:28:07 +0800

    test: add cluster test for mmseqs

--- test/cluster/test_mmseqs.py
+++ test/cluster/test_mmseqs.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 import pytest
 
 from protools.cluster import mmseqs
-from protools import utils
+from protools import utils, seqio
 try:
     mmseqs.MMseqs2()
     mmseqs_available = True
@@ -14,8 +15,15 @@ except utils.CmdNotFoundError as e:
 @pytest.mark.skipif(not mmseqs_available, reason=helper_msg)
 class TestMMseqs2():
 
-    @pytest.mark.xfail(reason="Not implemented")
-    def test_cluster():
-        # TODO
-        assert False
-    
+    def setup_method(self):
+        self.cmd = mmseqs.MMseqs2()
+
+    def test_cluster(self, tmp_path: Path):
+        data = seqio.read_fasta('data/test.fasta')
+        result = self.cmd.cluster(
+            seqs=data,
+            output=tmp_path / 'result.csv',
+            tmp_dir=tmp_path
+        )
+        assert len(result) == 3
+        assert (result.columns == ['cluster_id', 'sequence_id']).all()
